eca45e6f56c3ce686b5e5da2172d1a4cf7d8b2b1
H0ssamAhmed <hossamahmed8862@gmail.com>
2025-05-16 18:42:30 +0300

    change header to be globel

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -13,6 +13,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <header className="bg-primary text-white p-4 shadow-md">
+        <h1 className="text-2xl font-bold text-center">نظام متابعة الأجور</h1>
+      </header>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
